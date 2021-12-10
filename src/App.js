7791222025b6adc4bc6f285986512f245a252f9c
Zach Hornung <zachhornung@comcast.net>
2021-12-09 18:11:32 -0800

    working state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import TasksPage from './components/TasksPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    onCreateTask = ({ title, description }) => {
+        this.props.dispatch({
+            type: 'CREATE_TASK',
+            payload: {
+                title,
+                description,
+            }
+        });
+    }
+    render(){
+        return (
+            <div className="main-content">
+                <TasksPage 
+                    tasks={this.props.tasks}
+                    onCreateTask={this.onCreateTask}
+                />
+            </div>
+        );
+    }
 }
 
-export default App;
+function mapStateToProps(state){
+    return {
+        tasks: state.tasks
+    }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(tasks)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
